350cd65e2564693f53300825bd8c5b0c45a2ebad
Qroshi <34942734+Qroshi@users.noreply.github.com>
2018-09-25 19:32:57 +0200

    STM poprwana wersja

---
+++ STM/Debug/src/tm_stm32f4_lis302dl_lis3dsh.d
@@ -0,0 +1,112 @@
+src/tm_stm32f4_lis302dl_lis3dsh.o: ../src/tm_stm32f4_lis302dl_lis3dsh.c \
+ C:/Users/HP/workspace1/proj1/inc/tm_stm32f4_lis302dl_lis3dsh.h \
+ C:/Users/HP/workspace1/proj1/CMSIS/device/stm32f4xx.h \
+ C:/Users/HP/workspace1/proj1/CMSIS/core/core_cm4.h \
+ C:/Users/HP/workspace1/proj1/CMSIS/core/core_cmInstr.h \
+ C:/Users/HP/workspace1/proj1/CMSIS/core/core_cmFunc.h \
+ C:/Users/HP/workspace1/proj1/CMSIS/core/core_cm4_simd.h \
+ C:/Users/HP/workspace1/proj1/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/misc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_spi.h
+
+C:/Users/HP/workspace1/proj1/inc/tm_stm32f4_lis302dl_lis3dsh.h:
+
+C:/Users/HP/workspace1/proj1/CMSIS/device/stm32f4xx.h:
+
+C:/Users/HP/workspace1/proj1/CMSIS/core/core_cm4.h:
+
+C:/Users/HP/workspace1/proj1/CMSIS/core/core_cmInstr.h:
+
+C:/Users/HP/workspace1/proj1/CMSIS/core/core_cmFunc.h:
+
+C:/Users/HP/workspace1/proj1/CMSIS/core/core_cm4_simd.h:
+
+C:/Users/HP/workspace1/proj1/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_fsmc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/HP/workspace1/proj1/StdPeriph_Driver/inc/stm32f4xx_spi.h:
--- STM/src/main.c
+++ STM/src/main.c
@@ -2,14 +2,24 @@
 #include "stm32f4_discovery.h"
 #include "tm_stm32f4_lis302dl_lis3dsh.h"
 #include <stdio.h>
-#include <ctime>
+#include <stdlib.h>
+#include <math.h>
 int p=0;
+int x[4],y[4],z[4];
+int x_result,y_result,z_result;
+int x_max,y_max,z_max;
+int x_min,y_min,z_min;
+int MAX_VALUE=100;
+int x_th,y_th,z_th;
+int x_old,y_old,z_old;
+int x_new,y_new,z_new;
+int x_th_abs,y_th_abs,z_th_abs;
 TM_LIS302DL_LIS3DSH_t 	Axes_Data;
+
 int main(void)
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
@@ -38,17 +48,121 @@ int main(void)
 
 	for(;;){
 		TM_LIS302DL_LIS3DSH_ReadAxes(&Axes_Data);
-		if(Axes_Data.X<-6000&&p==0)
-		{
-			p++;
-		}
-		if(Axes_Data.X>6000&&p==1)
-		{
-			while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
-			USART_SendData(USART3, '1');
-			while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
-			p--;
-		}
-	}
-}
+            /*SUM FILTERING*/
+
+            /*NOT USED A LOOP FOR STM STUDIO*/
+            x[0] = Axes_Data.X;
+            y[0] = Axes_Data.Y;
+            z[0] = Axes_Data.Z;
+
+            x[1] = Axes_Data.X;
+            y[1] = Axes_Data.Y;
+            z[1] = Axes_Data.Z;
+
+            x[2] = Axes_Data.X;
+            y[2] = Axes_Data.Y;
+            z[2] = Axes_Data.Z;
+
+            x[3] = Axes_Data.X;
+            y[3] = Axes_Data.Y;
+            z[3] = Axes_Data.Z;
+
+            x_result = x[0] + x[1] + x[2] + x[3];
+            y_result = y[0] + y[1] + y[2] + y[3];
+            z_result = z[0] + z[1] + z[2] + z[3];
+            /*END SUM FILTERING*/
+
+            /*FIND MAX AND MIN VALUE*/
+            if (x_result > x_max) x_max = x_result;
+            if (x_result < x_min) x_min = x_result;
+
+            if (y_result > y_max) y_max = y_result;
+            if (y_result < y_min) y_min = y_result;
+
+            if (z_result > z_max) z_max = z_result;
+            if (z_result < z_min) z_min = z_result;
+            /*END FIND MAX AND MIN VALUE*/
+
+            p++;
+
+            if (p > 50) {
+                /*COMPUTE THRESHOLD*/
+                p = 0;
+                x_th = (x_max + x_min) / 2;
+                y_th = (y_max + y_min) / 2;
+                z_th = (z_max + z_min) / 2;
+
+                /*INIT VALUE*/
+                x_max = -MAX_VALUE;
+                x_min = MAX_VALUE;
+                y_max = -MAX_VALUE;
+                y_min = MAX_VALUE;
+                z_max = -MAX_VALUE;
+                z_min = MAX_VALUE;
+                /*END INIT VALUE*/
+            }
+
+            x_old = x_new;
+            y_old = y_new;
+            z_old = z_new;
+            /*DISCARD SMALL ACCEL*/
+            if (abs(x_new - x_result) > 100)
+                x_new = x_result;
+            if (abs(y_new - y_result) > 100)
+                y_new = y_result;
+            if (abs(z_new - z_result) > 100)
+                z_new = z_result;
+
+
+
+            /*FIND LARGEST ACCEL AXIS*/
+            x_th_abs = abs(x_th);
+            y_th_abs = abs(y_th);
+            z_th_abs = abs(z_th);
+
+            if (x_th_abs > y_th_abs) {
+                /*x > y*/
+                if (x_th_abs > z_th_abs) {
+
+                    /*x is the largest*/
+                    if (x_old > x_th && x_new < x_th)
+                    {
+            			while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+            			USART_SendData(USART3, '1');
+            			while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+                    }
+
+                } else {
+
+                    /*z is the largest*/
+                    if (z_old > z_th && z_new < z_th)
+                    {
+            			while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+            			USART_SendData(USART3, '1');
+            			while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+                    }
+
+                }
+            } else {
+                /*y > x*/
+                if (y_th_abs > z_th_abs) {
+                    /*y is the largest*/
+                    if (y_old > y_th && y_new < y_th)
+                    {
+            			while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+            			USART_SendData(USART3, '1');
+            			while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+                    }
+                } else {
+                    /*z is the largest*/
+                    if (z_old > z_th && z_new < z_th)
+                    {
+            			while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+            			USART_SendData(USART3, '1');
+            			while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+                    }
+                }
+            }
+        }
+    }
 
